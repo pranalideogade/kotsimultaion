5a4d4d6d9c0463a8ddad08b63d5d71f46297115b
pranali_deogade <pranali_deogade@persistent.co.in>
2017-12-15 15:29:14 +0530

    pran

---
+++ app/src/main/java/com/example/pranali_deogade/myapplication2/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.pranali_deogade.myapplication2
+
+/**
+ * Created by pranali_deogade on 08-12-2017.
+ */
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
